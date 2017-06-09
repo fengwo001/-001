fc29c60ac4c7fa1a8eadbdcb128d0b60f7d2c3b6
Administrator <Administrator@404notfound>
2017-06-09 11:53:44 +0800

    print（）

--- gittest/src/test/Test.java
+++ gittest/src/test/Test.java
@@ -4,6 +4,9 @@ public class Test {
 	public int sum(int x,int y){
 		return x+y;
 	}
+	public void print(){
+		System.out.println("但是一直太晚");
+	}
 	public static void main(String[] args) {
 	System.out.println("时间会给我答案");	
 	}
